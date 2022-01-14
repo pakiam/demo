4352e15f19f8ab64adea5c4eed1e86f46fd44923
Maksim Loginov <akiam@mail.ru>
2022-01-14 13:31:26 +0300

    remove some code from HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,24 +11,11 @@
       </v-col>
 
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
         <v-row justify="center">
-          <router-link :to="{ name: 'Programs'}">Программы</router-link>
+          <router-link :to="{ name: 'Programs'}">Перейти в магазин</router-link>
         </v-row>
       </v-col>
+
     </v-row>
   </v-container>
 </template>
